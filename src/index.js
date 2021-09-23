b7f6a060c87211ab21bcefdf52f00f5456f28f74
davidcachia29 <davidcachia29@gmail.com>
2021-09-23 09:08:42 +0200

    Final Project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './registerServiceWorker';
+import setupSocket from './sockets'
+import reducers from './reducers'
+import handleNewMessage from './sagas'
+import username from './utils/name'
+
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+	reducers,
+	applyMiddleware(sagaMiddleware)
+)
+
+const socket = setupSocket(store.dispatch, username)
+
+sagaMiddleware.run(handleNewMessage, {socket, username})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+	);
+registerServiceWorker();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
